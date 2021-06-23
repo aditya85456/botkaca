bb05abb1f24d82b75f3fab723076143bd3dd71dd
aditya85456 <86275783+aditya85456@users.noreply.github.com>
2021-06-23 10:07:27 +0530

    Update en.py

--- bot/locals/en.py
+++ bot/locals/en.py
@@ -2,7 +2,7 @@
 from bot.locals import Local
 
 LOCAL = Local({
-    'WRONG_ROOM' : 'I a\'m not suppose to be here.\nID : <code>{1001400005258}</code>',
+    'WRONG_ROOM' : 'I a\'m not suppose to be here.\nID : <code>{Chat_ID}</code>',
     'WELCOME_MESSAGE' : "Hi!\nI'm <b>Botkaca</b>!\nPowered by pyrogram.\nFork: <a href='https://github.com/Finitebottutu/botkaca'>github.com/Finitebottutu/botkaca</a>",
     'PASS_REQUIRED' : '\n\nUse <code>/{cmd_pass} </code>to enter the password.',
     'LEECH_LIST_MESSAGE_HEADER' : '<b>Leech Status</b>',
